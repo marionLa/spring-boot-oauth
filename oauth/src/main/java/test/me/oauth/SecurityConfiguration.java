f88dfff199f9ac5427bb7330c15e55ee1a15a270
Ixora <marionlabbe@posteo.net>
2025-01-06 09:45:35 +0100

    [ADD] Configuration de sécurité (SecurityFilterChain). Nécessite l'actualisation du WebSecurityAdapter déprécié depuis Spring Security 6.

---
+++ oauth/src/main/java/test/me/oauth/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package test.me.oauth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/", "/error", "/webjars/**").permitAll()
+                                .anyRequest().authenticated()
+                ).exceptionHandling(exceptionHandling ->
+                        exceptionHandling.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+                .oauth2Login(withDefaults());
+        return http.build();
+    }
+}
+
+
+
