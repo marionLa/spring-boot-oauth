59c527317f59f8c594de0dc9579650f5211e11c5
Ixora <marionlabbe@posteo.net>
2025-01-06 11:48:18 +0100

    [ADD] test de l'auth OAuth avec un utilisateur mock

---
+++ oauth/src/test/java/test/me/oauth/AuthUserTest.java
@@ -0,0 +1,58 @@
+package test.me.oauth;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import java.security.Principal;
+import java.util.Map;
+import java.util.Set;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class AuthUserTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void mustMockAuthenticatedUser() throws Exception {
+        Map<String, Object> attributes = Map.of(
+                "login", "mytestlogin"
+        );
+
+        OAuth2User oauth2User = new DefaultOAuth2User(
+                Set.of(new OAuth2UserAuthority(attributes)),
+                attributes,
+                "login"
+        );
+
+        OAuth2AuthenticationToken authenticationToken =
+                new OAuth2AuthenticationToken(oauth2User, oauth2User.getAuthorities(), "GitHub");
+
+        SecurityContext context = SecurityContextHolder.createEmptyContext();
+        context.setAuthentication(authenticationToken);
+        SecurityContextHolder.setContext(context);
+
+        mvc.perform(get("/").principal(authenticationToken))
+                .andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString("mytestlogin")));
+    }
+
+}
